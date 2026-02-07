4d05228818880eec6e0c257256143bb63eacefd8
bishopttw <bishopprinz6@gmail.com>
2026-02-07 05:20:58 +0100

    Added Todo.js And Modified App.js

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import Todo from "./components/Todo";
 
 export default function App() {
 
   const[newTodo, setNewTodo] = useState('');
   const[todos, setTodos] = useState([]);
+
   function handleInputchange(text){
     setNewTodo(text);
   }
 
   function handleAddTodo(){
     setTodos(todos => [...todos, newTodo]);
+  }
+
+  function handleDeleteTodo(id){
+    setTodos(ts => ts.filter(t => t.id !== id));
   }
 
   return (
@@ -22,13 +28,11 @@ export default function App() {
         <Button onPress={handleAddTodo} title="ADD TODO"/>
       </View>
 
-      <View style={styles.todoContainer}>
-        {
-          todos.map(todo => <View style={styles.todo} key={todo}>
-            <Text>{todo}</Text>
-          </View>)
-        }
-      </View>
+      <ScrollView>
+        <View style={styles.todoContainer}>
+          {todos.map(todo => <Todo onDelete={handleDeleteTodo} key={todo.id} todo={todo}/>)}
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -47,11 +51,5 @@ const styles = StyleSheet.create({
     gap:15,
     alignItems: 'center',
     paddingTop: 20
-  },
-  todo: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 10,
-    width: '80%',
   }
 });
---
+++ components/Todo.js
@@ -0,0 +1,21 @@
+import {Pressable, StyleSheet, Text, View} from "react-native";
+
+export default function Todo({todo, onDelete}) {
+    return(
+        <Pressable onPress={() => onDelete(todo.id)} style={styles.todo}>
+            <View>
+                <Text>{todo.todo}</Text>
+            </View>
+        </Pressable>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    todo: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 10,
+    width: '80%',
+  }
+});
