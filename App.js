8ed9f0932d0fa874611db0a23cf73fa3db2266b5
bishopttw <bishopprinz6@gmail.com>
2026-02-10 04:31:52 +0100

    Modified App.js and  Added background image

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, ScrollView, StyleSheet, Text, TextInput, View, Image, ImageBackground } from 'react-native';
 import Todo from "./components/Todo";
+import { SafeAreaView } from 'react-native/types_generated/index';
 
 export default function App() {
 
   const[newTodo, setNewTodo] = useState('');
   const[todos, setTodos] = useState([]);
 
+  let counter = 0;
+
   function handleInputchange(text){
+    const  td = {todo: text, id: counter};
     setNewTodo(text);
   }
 
   function handleAddTodo(){
     setTodos(todos => [...todos, newTodo]);
+
+    Alert.alert(
+      'Add Todo',
+      'Todo added successfully!',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel'
+        }
+      ],
+    );
   }
 
   function handleDeleteTodo(id){
@@ -21,7 +36,10 @@ export default function App() {
   }
 
   return (
-    <View>
+    <SafeAreaView style={styles.app}>
+    <View style ={styles.background}>
+      <StatusBar style="auto"/>
+      <ImageBackground style={styles.app} source={require('./assets/images/background.jpg')}>
       <View style={styles.inputcontainer}>
         <TextInput onChangeText={handleInputchange}
         style={styles.inputStyle}/>
@@ -33,11 +51,24 @@ export default function App() {
           {todos.map(todo => <Todo onDelete={handleDeleteTodo} key={todo.id} todo={todo}/>)}
         </View>
       </ScrollView>
+
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('./assets/images/normalImg.jpg')}/>
+      </View>
+
+      </ImageBackground>
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  app:{
+    flex: 1,
+  },
+  background:{
+    flex: 1,
+  },
   inputcontainer: {
     flexDirection: 'row',
     marginTop: 50
@@ -48,8 +79,19 @@ const styles = StyleSheet.create({
     fontSize: 25
   },
   todoContainer:{
+    
     gap:15,
     alignItems: 'center',
     paddingTop: 20
+  },
+  imageContainer:{
+    width: 200,
+    height: 200,
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  image:{
+    width: '100%',
+    height: '100%'
   }
 });
